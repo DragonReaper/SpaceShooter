aef19f0622279256d77f67a444142671b86e5a50
Will Tesalona <wstesalona@github.com>
2021-04-21 17:33:10 +0800

    wews try lang

--- wearforward/src/App.js
+++ wearforward/src/App.js
@@ -19,7 +19,7 @@ firebase.initializeApp({
 
 const auth = firebase.auth();
 const firestore = firebase.firestore();
-const analytics = firebase.analytics();
+//const analytics = firebase.analytics();
 
 function App() {
   const [user] = useAuthState(auth);
