c3c876b8dd070f61441f925f0e73c8d590dc8811
Will Tesalona <wstesalona@github.com>
2021-04-22 16:54:51 +0800

    Eto bago

--- wearforward/src/login.js
+++ wearforward/src/login.js
@@ -11,12 +11,24 @@ function Login(){
         <div className="form">
             <div className="box">
             <h3>LOGIN</h3>
-            <p>If you have an account, please log in.</p>
+            <p className="sentence1">If you have an account, please log in.</p>
             <ul>
                 <li><input className="input" type= "text" name= "Email" placeholder = "Email"></input></li>
                 <li><input className="input" type= "password" name= "Password" placeholder = "Password"></input></li>
                 <li><a href="">Forgot Password</a></li>
             </ul>
+            </div>
+            <div>
+                <p className="wala"> </p>
+            </div>
+            <div className="box">
+            <h3>FIRST TIME IN WEAR FORWARD?</h3>
+            <p className="sentence2">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque lectus
+ante, dapibus gravida diam eget, cursus porta nibh. rcu. Phasellus
+porttitor, mauris vitae consequat consequat, mauris urna feugiat lorem,
+et lacinia nunc dui vitae purus.</p>
+            <p className="sentence3">If you have any concerns, please email us at (wear forward email).</p>
+            <button className="button">CREATE AN ACCOUNT</button>
             </div>
         </div>
         </body>
