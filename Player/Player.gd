c7e8a31bd3b7394bcb60103e85bd253d0742ca7f
DragonReaper <2019sha01241@iacademy.edu.ph>
2021-07-19 14:12:56 +0800

    bullets added

--- Player/Player.gd
+++ Player/Player.gd
@@ -2,8 +2,11 @@ extends Area2D
 
 var plBullet := preload("res://Bullet/Bullet.tscn")
 onready var sprite:= $Sprite
+onready var firingPositions:=$FiringPositions
+onready var fireDelayTimer:= $FireDelayTimer
 
-var speed: float=100
+export var speed: float=100
+export var fireDelay: float= 0.1
 var vel := Vector2(0,0)
 
 func _process(delta):
@@ -13,15 +16,14 @@ func _process(delta):
 		sprite.frame=2
 	else:
 		sprite.frame=1
-		
-	# Check if bullets are shot
-	if Input.is_action_pressed("shoot"):
-		var bullet= plBullet.instance()
-		bullet.position = position
-		get_tree().current_scene.add_child(bullet)
 	
+	if Input.is_action_pressed("shoot") and fireDelayTimer.is_stopped():
+		fireDelayTimer.start(fireDelay)
+		for child in firingPositions.get_children():
+			var bullet := plBullet.instance()
+			bullet.global_position = child.global_position
+			get_tree().current_scene.add_child(bullet)
 
-	
 func _physics_process(delta):
 	var dirVec := Vector2(0,0)
 	
