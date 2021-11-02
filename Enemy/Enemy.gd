89ea2e00431ce2648a9304612c81be5b774da858
DragonReaper <2019sha01241@iacademy.edu.ph>
2021-11-03 07:00:18 +0800

    first message

--- Enemy/Enemy.gd
+++ Enemy/Enemy.gd
@@ -1,12 +1,17 @@
 extends Area2D
+class_name Enemy
 
 export var verticalSpeed := 10.0
 export var health : int = 5
+var playerInArea: Player= null
 
 func _physics_process(delta):
 	position.y += verticalSpeed * delta
 	
-	
+func _process(delta):
+	if playerInArea != null:
+		playerInArea.damage(1)
+		
 func damage(amount : int):
 	health -= amount
 	if health<= 0:
@@ -15,3 +20,12 @@ func damage(amount : int):
 
 func _on_VisibilityNotifier2D_screen_exited():
 	queue_free()
+
+
+func _on_Enemy_area_entered(area):
+	if area is Player:
+		playerInArea = area 
+
+func _on_Enemy_area_exited(area):
+	if area is Player:
+		playerInArea = null
